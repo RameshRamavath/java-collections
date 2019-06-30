dab1369891ae98c01066b99a06656d0033d1ea57
RameshRamavath <naik.iitkgp@gmail.com>
2019-06-30 20:56:25 +0530

    keyInterfaces commit

---
+++ src/main/java/com/java/collections/theory/keyInterfaces.java
@@ -0,0 +1,41 @@
+package com.java.collections.theory;
+
+public class keyInterfaces {
+
+/*
+
+Interfaces will always provide more info compared to classes -- classes are just dummy implementations of Interface. Always look for Interface definition!
+
+9 key Interfaces in Collection framework
+
+1. Collection:
+
+   when you want to represent group of objects into single entity use this interface
+   most common methods used in all other collection interfaces --> add, remove, search and check if empty collection or not etc
+
+   this can be considered as root interface of collection framework
+
+   collection vs collections?
+
+   --> collections is utility class present in java.util.package to define several utility methods like sorting, searching etc on Collection Objects
+
+2. List:
+
+   When duplicate elements allowed & insertion order is important go for List interface
+
+   Collection [parent interface] --> List [child interface] --> implemented classes for List interface are:
+
+   1. Array List
+   2. Linked List
+   3. Vector [parent utility class] --> 4. stack [child class]
+
+
+3. Set:
+
+   When we don't need duplicate and insertion order is not required -- go for Set interface
+   Collection [parent interface] --> Set [child interface] --> implemented classes for Set interface are:
+
+   1. HashSet [parent class] --> 2. linkedHashSet [child class]
+
+ */
+}
